e228fc4f8e69b9befe916c8c1ea206b4fd0ee370
G26-X <1727970609@qq.com>
2021-03-22 13:56:41 +0800

    添加项目文件。

---
+++ 前端/Form1.Designer.cs
@@ -0,0 +1,301 @@
+﻿namespace 前端
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		/// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows 窗体设计器生成的代码
+
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要修改
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		private void InitializeComponent()
+		{
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.splitter1 = new System.Windows.Forms.Splitter();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.splitter2 = new System.Windows.Forms.Splitter();
+            this.videoSourcePlayer1 = new AForge.Controls.VideoSourcePlayer();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel4);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(837, 714);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.panel4.Controls.Add(this.button5);
+            this.panel4.Controls.Add(this.button1);
+            this.panel4.Controls.Add(this.button4);
+            this.panel4.Controls.Add(this.button3);
+            this.panel4.Location = new System.Drawing.Point(21, 506);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(796, 53);
+            this.panel4.TabIndex = 8;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button5.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button5.Location = new System.Drawing.Point(707, 9);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(67, 34);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "举手";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::前端.Properties.Resources._0;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button1.Location = new System.Drawing.Point(31, 5);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(47, 42);
+            this.button1.TabIndex = 3;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button4.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button4.Location = new System.Drawing.Point(588, 9);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 34);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "会议成员";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button3.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(467, 9);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 34);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "共享屏幕";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(52, 580);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 36);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "结束会议";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 23);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(796, 486);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // splitter1
+            // 
+            this.splitter1.BackColor = System.Drawing.Color.GhostWhite;
+            this.splitter1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitter1.Location = new System.Drawing.Point(837, 0);
+            this.splitter1.Name = "splitter1";
+            this.splitter1.Size = new System.Drawing.Size(6, 714);
+            this.splitter1.TabIndex = 1;
+            this.splitter1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Controls.Add(this.textBox1);
+            this.panel2.Controls.Add(this.button6);
+            this.panel2.Controls.Add(this.textBox2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(843, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(234, 714);
+            this.panel2.TabIndex = 2;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.button8);
+            this.panel3.Controls.Add(this.button7);
+            this.panel3.Controls.Add(this.splitter2);
+            this.panel3.Controls.Add(this.videoSourcePlayer1);
+            this.panel3.Location = new System.Drawing.Point(-6, 3);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(239, 230);
+            this.panel3.TabIndex = 3;
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button8.Font = new System.Drawing.Font("微软雅黑", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button8.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button8.Location = new System.Drawing.Point(141, 179);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(73, 30);
+            this.button8.TabIndex = 4;
+            this.button8.Text = "停止拍照";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.button7.Font = new System.Drawing.Font("微软雅黑", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button7.Location = new System.Drawing.Point(25, 179);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(73, 30);
+            this.button7.TabIndex = 3;
+            this.button7.Text = "开始拍照";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // splitter2
+            // 
+            this.splitter2.BackColor = System.Drawing.Color.GhostWhite;
+            this.splitter2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.splitter2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.splitter2.Location = new System.Drawing.Point(0, 224);
+            this.splitter2.Name = "splitter2";
+            this.splitter2.Size = new System.Drawing.Size(239, 6);
+            this.splitter2.TabIndex = 0;
+            this.splitter2.TabStop = false;
+            // 
+            // videoSourcePlayer1
+            // 
+            this.videoSourcePlayer1.Location = new System.Drawing.Point(9, 3);
+            this.videoSourcePlayer1.Name = "videoSourcePlayer1";
+            this.videoSourcePlayer1.Size = new System.Drawing.Size(221, 215);
+            this.videoSourcePlayer1.TabIndex = 5;
+            this.videoSourcePlayer1.Text = "videoSourcePlayer1";
+            this.videoSourcePlayer1.VideoSource = null;
+            this.videoSourcePlayer1.NewFrame += new AForge.Controls.VideoSourcePlayer.NewFrameHandler(this.videoSourcePlayer1_NewFrame);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(6, 239);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(218, 335);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.button6.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.Location = new System.Drawing.Point(149, 662);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(59, 31);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "发送";
+            this.button6.UseVisualStyleBackColor = false;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(6, 580);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(218, 122);
+            this.textBox2.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.ClientSize = new System.Drawing.Size(1077, 714);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.splitter1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "手势识别";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.Splitter splitter1;
+		private System.Windows.Forms.Panel panel2;
+		private System.Windows.Forms.Panel panel3;
+		private System.Windows.Forms.Splitter splitter2;
+		private System.Windows.Forms.TextBox textBox2;
+		private System.Windows.Forms.TextBox textBox1;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.Panel panel4;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button4;
+		private System.Windows.Forms.Button button3;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button6;
+		private System.Windows.Forms.Button button7;
+		private System.Windows.Forms.Button button8;
+        private AForge.Controls.VideoSourcePlayer videoSourcePlayer1;
+    }
+}
+
---
+++ 前端/Form1.cs
@@ -0,0 +1,112 @@
+﻿using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 前端
+{
+	public partial class Form1 : Form
+	{
+        System.Timers.Timer atimer;//自定义timer 这个timer会在interval到期而触发事件还未执行完毕时，创建一个新的线程来执行新的触发事件
+        FilterInfoCollection videoDevices;//设备集合
+        VideoCaptureDevice videoSource;//设备源
+        public Form1()
+		{
+			InitializeComponent();
+            cameraOn();//打开摄像头
+            InitTimer();//初始化timer
+		}
+        //麦克风图标 点击会有开麦和闭麦的图片切换
+		int voiceclick = 0;
+		private void button1_Click(object sender, EventArgs e)
+		{
+			if(voiceclick == 0)
+			{
+				voiceclick = 1;
+				button1.BackgroundImage = Properties.Resources._1;
+			}
+			else if(voiceclick == 1)
+			{
+				voiceclick = 0;
+				button1.BackgroundImage = Properties.Resources._0;
+			}
+		}
+        //打开摄像头
+        private void cameraOn() {
+            videoDevices = new FilterInfoCollection(FilterCategory.VideoInputDevice);//获取所有设备
+            videoSource = new VideoCaptureDevice(videoDevices[0].MonikerString);//连接摄像头
+            //多个摄像头时无法选择，默认使用程序识别出的第一个摄像头
+            this.videoSourcePlayer1.VideoSource = videoSource;//videoSourcePlayer1是显示视频画面的一个Aforge控件
+            this.videoSourcePlayer1.Start();//启动
+        }
+        //关闭摄像头
+        private void cameraOff() {
+            this.videoSourcePlayer1.SignalToStop();
+        }
+        //关闭窗口时关闭摄像头
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            cameraOff();
+        }
+        //获取当前帧图片
+        private void takePhoto()
+        {
+            //定义文件名
+            string imgName = DateTime.Now.ToString("yyyyMMddHHmmssfff");
+            string path = @"D:\videocamera\WindowsFormsApp1\images\" + imgName + ".jpg";
+            if (videoSourcePlayer1 == null)
+            {
+                return;
+            }
+            //获取当前帧图片
+            Bitmap bitmap = this.videoSourcePlayer1.GetCurrentVideoFrame();
+            //保存图片
+            bitmap.Save(path);
+        }
+        //初始化自定义timer
+        private void InitTimer()
+        {
+            //设置定时间隔(毫秒为单位)
+            int interval = 100;
+            atimer = new System.Timers.Timer(interval);
+            //设置执行一次（false）还是一直执行(true)
+            atimer.AutoReset = true;
+            //设置是否执行System.Timers.Timer.Elapsed事件
+            atimer.Enabled = true;
+            //绑定Elapsed事件
+            atimer.Elapsed += new System.Timers.ElapsedEventHandler(callback);
+
+        }
+        //设置回调函数
+        private void callback(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            takePhoto();
+        }
+        //开始拍照----定时器启动
+        private void button7_Click(object sender, EventArgs e)
+        {
+            atimer.Start();
+        }
+        //停止拍照----定时器关闭
+        private void button8_Click(object sender, EventArgs e)
+        {
+            atimer.Stop();
+        }
+        //使视频镜像
+        private void videoSourcePlayer1_NewFrame(object sender, ref Bitmap image)
+        {
+            if (image != null)
+            {
+                image.RotateFlip(RotateFlipType.RotateNoneFlipX);
+            }
+        }
+
+        
+    }
+}
